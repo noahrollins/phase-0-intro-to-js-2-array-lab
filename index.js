d7e5d25f4a62e49228ffd29161882a0ae8074697
Noah Rollins <noahjrollins@gmail.com>
2022-11-01 15:40:48 -0500

    commit tree 650a2f242807ca37702ab1aa5e6ae1bc9c52cdd2

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
-// Write your solution here!
+const cats = [
+    "Milo",
+    "Otis",
+    "Garfield",
+
+]
+
+function destructivelyAppendCat(name) {
+    cats.push("Ralph")
+}
+
+function destructivelyPrependCat(name){
+    cats.unshift("Bob")
+};
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function appendCat(name) {
+    return [...cats, "Broom"];
+}
+
+function prependCat(name) {
+    return ["Arnold", ...cats];
+}
+
+function removeLastCat() {
+    return cats.slice(0,cats.length-1);
+}
+
+function removeFirstCat() {
+    return cats.slice(1)
+}
